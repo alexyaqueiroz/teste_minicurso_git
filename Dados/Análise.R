acf0e732b995e69e01f74c76af8a6bd7e7494e54
alexyaqueiroz <alexyaqueiroz@hotmail.com>
2024-05-24 11:15:09 -0300

    Criação de um dataframe e um gráfico

--- Dados/Análise.R
+++ Dados/Análise.R
@@ -1,3 +1,15 @@
-# Este script faz uma análise bem simples
+# Este script faz uma análise simples
 library(tidyverse)
 x = -10:10
+y <- x^2
+
+#Criar um dataframe
+
+df <- data.frame(x, y)
+
+#Vamos criar um gráfico
+
+df %>% 
+  ggplot(aes(x, y)) +
+  geom_point(col = "red", size = 4) +
+  geom_line(col = "green")
